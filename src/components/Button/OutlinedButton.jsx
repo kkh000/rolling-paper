9890166f2bcc415fbe048ba6d3049d9078df355d
민영 <alsdyd1021@naver.com>
2024-02-29 02:54:45 +0900

    feat: outlined button UI 구현

--- src/components/Button/OutlinedButton.jsx
+++ src/components/Button/OutlinedButton.jsx
@@ -1,15 +1,25 @@
-// import { useState } from 'react';
+import { EMOJI_ICON_PATH, DELETE_ICON_PATH } from '../../constant/constant';
 import css from './OutlinedButton.module.scss';
 
 const OutlinedButton = () => {
-  // hasIcon OutlineSize = prop
+  // prop : hasIcon={true/false}, type={'text'/'delete'}, outlineSize={outline56/40/36/28}
+  const hasIcon = true;
+  const type = 'text';
+
   // add-lg-white, deleted-white : disabled, 나머지 add-lg, deleted
 
   return (
     <div>
-      <button className={css.button}>
-        <span className={css.buttonTitle}>버튼버튼</span>
-      </button>
+      {type === 'text' ? (
+        <button className={css.outlined40}>
+          {hasIcon && <img className={css.emojiIcon} src={EMOJI_ICON_PATH} />}
+          <p className={css.buttonTitle}>Enabled</p>
+        </button>
+      ) : (
+        <button className={css.deleted}>
+          <img src={DELETE_ICON_PATH} />
+        </button>
+      )}
     </div>
   );
 };
---
+++ src/constant/constant.js
@@ -0,0 +1,5 @@
+// OutlinedButton
+export const EMOJI_ICON_PATH = 'assets/add-lg.svg';
+export const EMOJI_WHITE_ICON_PATH = 'assets/add-lg-white.svg';
+export const DELETE_ICON_PATH = 'assets/deleted.svg';
+export const DELETE_WHITE_ICON_PATH = 'assets/deleted-white.svg';
