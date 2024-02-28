78d0faee69c5ce828198def2951557b427d733ca
Junyoung Park <9851248@gmail.com>
2024-02-28 16:06:21 +0900

    refactor: App.jsx 함수 선언형 -> 화살표 함수 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { Outlet } from 'react-router-dom';
 import css from './App.module.scss';
 import Header from './components/Header/Header';
 
-function App() {
+const App = () => {
   return (
     <div className={css.layout}>
       <Header />
       <Outlet />
     </div>
   );
-}
+};
 
 export default App;
