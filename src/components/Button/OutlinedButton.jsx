7cb59be054ea0817ede1c963e3deab126888a1b3
민영 <alsdyd1021@naver.com>
2024-02-29 16:24:52 +0900

    refactor: DeletedButton, OutlinedButton 컴포넌트 분리

--- src/components/Button/OutlinedButton.jsx
+++ src/components/Button/OutlinedButton.jsx
@@ -1,39 +1,25 @@
-import {
-  EMOJI_ICON_PATH,
-  EMOJI_WHITE_ICON_PATH,
-  DELETE_ICON_PATH,
-  DELETE_WHITE_ICON_PATH,
-} from '../../constant/constant';
+import { EMOJI_ICON_PATH, EMOJI_WHITE_ICON_PATH } from '../../constant/constant';
 import css from './OutlinedButton.module.scss';
 
 const OutlinedButton = ({
-  isDeleted,
   isDisabled = false,
-  hasIcon = false,
-  size = 'Xl',
+  hasIcon = true,
+  size = 'small',
   children = 'Enabled',
   onClick,
 }) => {
-  const buttonClassName = isDeleted ? css.deleted : css['outlined' + size];
-
   return (
-    <div>
-      {isDeleted ? (
-        <button className={buttonClassName}>
-          <img src={isDisabled ? DELETE_WHITE_ICON_PATH : DELETE_ICON_PATH} />
-        </button>
-      ) : (
-        <button className={buttonClassName} disabled={isDisabled} onClick={onClick}>
-          {hasIcon && (
-            <img
-              className={css.emojiIcon}
-              src={isDisabled ? EMOJI_WHITE_ICON_PATH : EMOJI_ICON_PATH}
-            />
-          )}
-          <p className={css.buttonTitle}>{children}</p>
-        </button>
-      )}
-    </div>
+    <section>
+      <button className={css[size]} disabled={isDisabled} onClick={onClick}>
+        {hasIcon && (
+          <img
+            className={css.emojiIcon}
+            src={isDisabled ? EMOJI_WHITE_ICON_PATH : EMOJI_ICON_PATH}
+          />
+        )}
+        <p className={css.buttonTitle}>{children}</p>
+      </button>
+    </section>
   );
 };
 
