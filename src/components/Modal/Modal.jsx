1612ef7f3091deece7efadd0a4d0ef5ff1013000
unknown <gyooheon@naver.com>
2024-02-29 14:42:18 +0900

    refactor : px을 rem으로 변환

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import reactDom from 'react-dom';
+import Badge from '../../../../copy/src/components/Badge/Badge';
+import css from './Modal.module.scss';
+
+const Modal = ({ profileImage, creatorName, relationship, createdAt, onClose, message }) => {
+  return reactDom.createPortal(
+    <div className={css.background} onClick={onClose}>
+      <div
+        className={css.layout}
+        onClick={e => {
+          e.stopPropagation();
+        }}
+      >
+        <div className={css.informationBox}>
+          <div className={css.profileBox}>
+            <img className={css.profileImage} src={profileImage} />
+            <div className={css.profileTitle}>
+              From. <span className={css.profileName}>{creatorName}</span>
+            </div>
+            <Badge className={css.profileBadge} relationship={relationship} />
+          </div>
+          <div className={css.cardCreatedAt}>{createdAt}</div>
+        </div>
+        <div className={css.line}></div>
+        <div className={css.textBox}>{message}</div>
+        <button className={css.button} onClick={onClose}>
+          확인
+        </button>
+      </div>
+    </div>,
+    document.getElementById('modal'),
+  );
+};
+
+export default Modal;
