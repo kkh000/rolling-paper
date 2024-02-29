dccdd97542c1c22069a738a89b89346817740c1d
김규헌 <105029085+kkh000@users.noreply.github.com>
2024-02-29 16:31:12 +0900

    태그 변경

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ import css from './Modal.module.scss';
 
 const Modal = ({ profileImage, creatorName, relationship, createdAt, onClose, message }) => {
   return reactDom.createPortal(
-    <div className={css.background} onClick={onClose}>
+    <section className={css.background} onClick={onClose}>
       <div
         className={css.layout}
         onClick={e => {
@@ -14,20 +14,20 @@ const Modal = ({ profileImage, creatorName, relationship, createdAt, onClose, me
         <div className={css.informationBox}>
           <div className={css.profileBox}>
             <img className={css.profileImage} src={profileImage} />
-            <div className={css.profileTitle}>
+            <h1 className={css.profileTitle}>
               From. <span className={css.profileName}>{creatorName}</span>
-            </div>
+            </h1>
             <Badge className={css.profileBadge} relationship={relationship} />
           </div>
           <div className={css.cardCreatedAt}>{createdAt}</div>
         </div>
         <div className={css.line}></div>
-        <div className={css.textBox}>{message}</div>
+        <p className={css.textBox}>{message}</p>
         <button className={css.button} onClick={onClose}>
           확인
         </button>
       </div>
-    </div>,
+    </section>,
     document.getElementById('modal'),
   );
 };
