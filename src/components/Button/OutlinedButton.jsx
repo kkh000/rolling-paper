d467241b21976b941f894cb87537377766bd9ebd
민영 <alsdyd1021@naver.com>
2024-02-29 14:29:28 +0900

    refactor: type 조건에서 isDeleted 조건으로 변경, size prop으로 변경

--- src/components/Button/OutlinedButton.jsx
+++ src/components/Button/OutlinedButton.jsx
@@ -6,24 +6,31 @@ import {
 } from '../../constant/constant';
 import css from './OutlinedButton.module.scss';
 
-const OutlinedButton = ({ isDisabled, hasIcon, type, outlinedSize, text }) => {
-  const buttonClassName = type === 'text' ? css[outlinedSize] : css.deleted;
+const OutlinedButton = ({
+  isDeleted,
+  isDisabled = false,
+  hasIcon = false,
+  size = 'Xl',
+  children = 'Enabled',
+  onClick,
+}) => {
+  const buttonClassName = isDeleted ? css.deleted : css['outlined' + size];
 
   return (
     <div>
-      {type === 'text' ? (
-        <button className={buttonClassName} disabled={isDisabled}>
+      {isDeleted ? (
+        <button className={buttonClassName}>
+          <img src={isDisabled ? DELETE_WHITE_ICON_PATH : DELETE_ICON_PATH} />
+        </button>
+      ) : (
+        <button className={buttonClassName} disabled={isDisabled} onClick={onClick}>
           {hasIcon && (
             <img
               className={css.emojiIcon}
               src={isDisabled ? EMOJI_WHITE_ICON_PATH : EMOJI_ICON_PATH}
             />
           )}
-          <p className={css.buttonTitle}>{text}</p>
-        </button>
-      ) : (
-        <button className={buttonClassName}>
-          <img src={isDisabled ? DELETE_WHITE_ICON_PATH : DELETE_ICON_PATH} />
+          <p className={css.buttonTitle}>{children}</p>
         </button>
       )}
     </div>
