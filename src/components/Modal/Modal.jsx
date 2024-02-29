7bc6dd3e86bc6405070b3050c0a7629f83a179b3
unknown <gyooheon@naver.com>
2024-02-29 16:55:28 +0900

    refactor: 화면 스크롤 방지 기능 추가

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,31 @@
+import { useEffect } from 'react';
 import reactDom from 'react-dom';
 import Badge from '../../../../copy/src/components/Badge/Badge';
 import css from './Modal.module.scss';
 
 const Modal = ({ profileImage, creatorName, relationship, createdAt, onClose, message }) => {
+  const preventScroll = () => {
+    const currentScrollY = window.scrollY;
+    document.body.style.position = 'fixed';
+    document.body.style.width = '100%';
+    document.body.style.top = `-${currentScrollY}px`;
+    document.body.style.overflowY = 'scroll';
+    return currentScrollY;
+  };
+
+  const allowScroll = prevScrollY => {
+    document.body.style.position = '';
+    document.body.style.width = '';
+    document.body.style.top = '';
+    document.body.style.overflowY = '';
+    window.scrollTo(0, prevScrollY);
+  };
+  useEffect(() => {
+    const prevScrollY = preventScroll();
+    return () => {
+      allowScroll(prevScrollY);
+    };
+  }, []);
   return reactDom.createPortal(
     <section className={css.background} onClick={onClose}>
       <div
