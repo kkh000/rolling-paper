61f4072c925447bbbe408ccf7026c753b94a3e61
Junyoung Park <9851248@gmail.com>
2024-02-29 20:00:57 +0900

    refactor: 스크롤 함수 외부 utils함수로 전환

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,10 @@
 import { useEffect } from 'react';
 import reactDom from 'react-dom';
 import Badge from '../../../../copy/src/components/Badge/Badge';
+import { allowScroll, preventScroll } from '../../utils/scrolls';
 import css from './Modal.module.scss';
 
 const Modal = ({ profileImage, creatorName, relationship, createdAt, onClose, message }) => {
-  const preventScroll = () => {
-    const currentScrollY = window.scrollY;
-    document.body.style.position = 'fixed';
-    document.body.style.width = '100%';
-    document.body.style.top = `-${currentScrollY}px`;
-    document.body.style.overflowY = 'scroll';
-    return currentScrollY;
-  };
-
-  const allowScroll = prevScrollY => {
-    document.body.style.position = '';
-    document.body.style.width = '';
-    document.body.style.top = '';
-    document.body.style.overflowY = '';
-    window.scrollTo(0, prevScrollY);
-  };
   useEffect(() => {
     const prevScrollY = preventScroll();
     return () => {
