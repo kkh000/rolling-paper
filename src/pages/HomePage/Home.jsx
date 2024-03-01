19214395aaf204a09cd095422ccede1fb8ecbccf
민영 <alsdyd1021@naver.com>
2024-03-02 03:26:50 +0900

    feat: 메인페이지 구현

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,28 +1,35 @@
+import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import { MAIN_IMAGE_01, MAIN_IMAGE_02 } from '../../constant/constant';
-import './Home.module.scss';
+import css from './Home.module.scss';
 
 const Home = () => {
   return (
-    <>
-      <div className='point01'>
-        <div className='textBox'>
-          <p>point. 01</p>
-          <h1>누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요</h1>
-          <p>로그인 없이 자유롭게 만들어요.</p>
+    <section className={css.layout}>
+      <article className={css.point01}>
+        <div className={css.textBox}>
+          <p className={css.pointNumber}>Point. 01</p>
+          <div className={css.titleBox}>
+            <h1 className={css.title}>누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요</h1>
+            <p className={css.description}>로그인 없이 자유롭게 만들어요.</p>
+          </div>
         </div>
-        <img src={MAIN_IMAGE_01} alt='롤링 소개 이미지' />
-      </div>
-      <div className='point02'>
-        <p>point. 02</p>
-        <div className='textBox'>
-          <h1>서로에게 이모지로 감정을 표현해보세요</h1>
-          <p>롤링 페이퍼에 이모지를 추가할 수 있어요.</p>
+        <img className={css.mainImage} src={MAIN_IMAGE_01} alt='롤링 소개 이미지' />
+      </article>
+      <article className={css.point02}>
+        <div className={css.textBox}>
+          <p className={css.pointNumber}>Point. 02</p>
+          <div className={css.titleBox}>
+            <h1 className={css.title}>서로에게 이모지로 감정을 표현해보세요</h1>
+            <p className={css.description}>롤링 페이퍼에 이모지를 추가할 수 있어요.</p>
+          </div>
         </div>
-        <img src={MAIN_IMAGE_02} alt='롤링 소개 이미지' />
-      </div>
-      <Button>구경해보기</Button>
-    </>
+        <img className={css.mainImage} src={MAIN_IMAGE_02} alt='롤링 소개 이미지' />
+      </article>
+      <Link to='/list'>
+        <Button width={'280px'}>구경해보기</Button>
+      </Link>
+    </section>
   );
 };
 
