a5e56c2352d3ff7a81e94a862100e15e13a0676a
unknown <gyooheon@naver.com>
2024-03-02 13:30:32 +0900

    fix: constant path 절대경로로 수정

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,19 +1,19 @@
-export const CHECK_ICON = 'assets/completed.svg';
-export const CLOSE_ICON = 'assets/close.svg';
+export const CHECK_ICON = '/assets/completed.svg';
+export const CLOSE_ICON = '/assets/close.svg';
 
-export const ARROW_UP_ICON = 'assets/arrow_up.svg';
-export const ARROW_DOWN_ICON = 'assets/arrow_down.svg';
+export const ARROW_UP_ICON = '/assets/arrow_up.svg';
+export const ARROW_DOWN_ICON = '/assets/arrow_down.svg';
 
-export const EMOJI_ICON_PATH = 'assets/add-lg.svg';
-export const EMOJI_WHITE_ICON_PATH = 'assets/add-lg-white.svg';
+export const EMOJI_ICON_PATH = '/assets/add-lg.svg';
+export const EMOJI_WHITE_ICON_PATH = '/assets/add-lg-white.svg';
 
-export const DELETE_ICON_PATH = 'assets/deleted.svg';
-export const DELETE_WHITE_ICON_PATH = 'assets/deleted-white.svg';
+export const DELETE_ICON_PATH = '/assets/deleted.svg';
+export const DELETE_WHITE_ICON_PATH = '/assets/deleted-white.svg';
 
-export const NONE_PORFILE_IMAGE = 'assets/none-profile.svg';
+export const NONE_PORFILE_IMAGE = '/assets/none-profile.svg';
 
-export const PLUS_ICON = 'assets/plus.svg';
-export const SHARE_ICON = 'assets/share.svg';
+export const PLUS_ICON = '/assets/plus.svg';
+export const SHARE_ICON = '/assets/share.svg';
 
 export const CARD_COLOR_LIST = ['orange', 'purple', 'blue', 'green'];
 export const BACKGROUND_IMAGE_URL_LIST = [
