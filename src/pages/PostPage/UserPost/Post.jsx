2fb4dda2cfb50af10fab314d374bcfd808253467
JA_X <envijs93@gmail.com>
2024-03-02 17:40:54 +0900

    Feat: Bar 컴포넌트 레이아웃 및 하위 컴포넌트 css 수정, Card 컴포넌트 UI 기초 기능 구현

--- src/pages/PostPage/UserPost/Post.jsx
+++ src/pages/PostPage/UserPost/Post.jsx
@@ -1,9 +1,24 @@
+import RoundedPlusButton from '../../../components/Button/RoundedPlusButton';
 import Bar from './Bar/Bar';
+import Card from './Bar/Card';
 import css from './Post.module.scss';
+
 const Post = () => {
   return (
     <div className={css.layout}>
       <Bar />
+      <div className={css.cardArea}>
+        <div className={css.cardBox}>
+          <div className={css.card}>
+            {/* onClick에 /post/{id}/message 로 가는 이벤트핸들러 넣어줘야됨 */}
+            <RoundedPlusButton />
+          </div>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
