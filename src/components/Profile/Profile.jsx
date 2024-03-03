858eaecaa4baf6655319191f4ad267fb31493ea0
Yeojin Yoon <roseiris1001@gmail.com>
2024-03-03 11:28:19 +0900

    feat:프로필 이미지에 size, z-index옵션을 줄 수 있도록 수정

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,26 @@
 import { NONE_PORFILE_IMAGE } from '../../constant/constant';
+import { cn } from '../../utils/classNames';
 import css from './Profile.module.scss';
 
 /**
- * 이미지 url을 받아서 프로필이미지를 생성해줍니다.
- * @param url
- * @returns div
+ * 이미지 imgUrl, size, zIndex를 받아서 프로필이미지를 생성해줍니다.
+ * imgUrl은 필수, 그 외는 선택가능합니다.
+ * @param {,imgUrl, size, zIndex}
+ * @returns
  */
-const Profile = ({ url }) => {
-  const imgUrl = url || NONE_PORFILE_IMAGE;
+const Profile = ({ imgUrl, size, zIndex }) => {
+  let imgUrlPath = imgUrl ?? NONE_PORFILE_IMAGE;
+  zIndex = `zIndex${zIndex}`;
+
+  const imageStatus = imgUrl ? 'profileImage' : 'profileNoneImage';
 
   return (
-    <div className={css.profile}>
-      <img className={url ? css.profileImage : css.profileNoneImage} src={imgUrl} alt='profile' />
-    </div>
+    <img className={cn(css[imageStatus], css[size], css[zIndex])} src={imgUrlPath} alt='profile' />
   );
+};
+
+Profile.defaultProps = {
+  size: 'medium',
 };
 
 export default Profile;
