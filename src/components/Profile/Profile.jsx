80202cb4e993b6002bac51b31f95183c126baa25
Yeojin Yoon <roseiris1001@gmail.com>
2024-03-03 13:51:26 +0900

    feat:프로필 이미지에서 size값으로 커스텀 가능하도록 수정

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,24 +3,17 @@ import { cn } from '../../utils/classNames';
 import css from './Profile.module.scss';
 
 /**
- * 이미지 imgUrl, size, zIndex를 받아서 프로필이미지를 생성해줍니다.
- * imgUrl은 필수, 그 외는 선택가능합니다.
- * @param {,imgUrl, size, zIndex}
+ * 이미지 imgUrl, size 받아서 프로필이미지를 생성해줍니다.
+ * imgUrl은 필수, size미입력시 default값 = medium
+ * @param {,imgUrl, size}
  * @returns
  */
-const Profile = ({ imgUrl, size, zIndex }) => {
+const Profile = ({ imgUrl, size = 'medium' }) => {
   let imgUrlPath = imgUrl ?? NONE_PORFILE_IMAGE;
-  zIndex = `zIndex${zIndex}`;
 
   const imageStatus = imgUrl ? 'profileImage' : 'profileNoneImage';
 
-  return (
-    <img className={cn(css[imageStatus], css[size], css[zIndex])} src={imgUrlPath} alt='profile' />
-  );
-};
-
-Profile.defaultProps = {
-  size: 'medium',
+  return <img className={cn(css[imageStatus], css[size])} src={imgUrlPath} alt='profile' />;
 };
 
 export default Profile;
