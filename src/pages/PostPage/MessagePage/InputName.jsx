b871c2780ac52970ed8ea9d94aa57c69160bfa48
unknown <gyooheon@naver.com>
2024-03-03 12:22:21 +0900

    feat : 포커스 아웃한 상태에서 값의 입력이 없을 시에 에러 발생

--- src/pages/PostPage/MessagePage/InputName.jsx
+++ src/pages/PostPage/MessagePage/InputName.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import Input from '../../../components/Input/Input';
 import css from './InputName.module.scss';
 
-const InputName = () => {
+const InputName = ({ onChange }) => {
+  const [error, setError] = useState(false);
+
+  const handleBlur = e => {
+    const value = e.target.value;
+    setError(value.trim() === '');
+    onChange(value);
+  };
+
   return (
-    <section className={css.box}>
+    <div className={css.box}>
       <h1 className={css.title}>From.</h1>
-      <Input placeholder='이름을 입력해 주세요.' isDisabled={false} />
-    </section>
+      <Input
+        placeholder='이름을 입력해 주세요.'
+        isDisabled={false}
+        isError={error}
+        onBlur={handleBlur}
+      />
+    </div>
   );
 };
 
