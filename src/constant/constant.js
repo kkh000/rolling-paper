992efe9d9c12a0684868b3be77554fe0b9a07469
zoonyoung <9851248@gmail.com>
2024-03-03 22:10:57 +0900

    feat: Add ShareOptioList

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,3 +1,6 @@
+import { shareKakao } from '../utils/kakao';
+import { copyToClipboard } from '../utils/share';
+
 export const CHECK_ICON = '/assets/completed.svg';
 export const CLOSE_ICON = '/assets/close.svg';
 
@@ -15,7 +18,6 @@ export const NONE_PORFILE_IMAGE = '/assets/none-profile.svg';
 export const PLUS_ICON = '/assets/plus.svg';
 export const SHARE_ICON = '/assets/share.svg';
 
-
 export const CARD_COLOR_LIST = ['orange', 'purple', 'blue', 'green'];
 export const BACKGROUND_IMAGE_URL_LIST = [
   'https://i.ibb.co/kgwVr13/xmas2.jpg',
@@ -29,3 +31,9 @@ export const AXIOS_CONTENT_TYPE = 'application/json';
 
 export const FONT_LIST = ['Noto Sans', 'Pretendara', '나눔명조', '나눔손글씨 손편지체'];
 export const RELATIONSHIP_LIST = ['지인', '친구', '동료', '가족'];
+
+export const SHARE_OPTION_LIST = [
+  { name: '카카오톡', handler: shareKakao },
+  { name: '링크', handler: copyToClipboard },
+];
+export const WEB_URL = 'https://www.naver.com';
--- src/pages/PostPage/UserPost/Bar/Bar.jsx
+++ src/pages/PostPage/UserPost/Bar/Bar.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import Toast from '../../../../components/Toast/Toast';
+import { SHARE_OPTION_LIST } from '../../../../constant/constant';
 import css from './Bar.module.scss';
 import Emoji from './Emoji';
 import Share from './Share';
@@ -8,6 +11,16 @@ const Bar = ({ recipientId, name }) => {
   //TODO 테스트 후 제거 필요
   console.log('::recipientId::', recipientId);
   name = '테스트';
+  const [showToast, setShowToast] = useState(false);
+
+  const handleShareSucess = () => {
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 5000);
+  };
+
+  const handleToastClose = () => {
+    setShowToast(false);
+  };
 
   return (
     <div className={css.barArea}>
@@ -15,7 +28,14 @@ const Bar = ({ recipientId, name }) => {
       <div className={css.barContents}>
         <WritingCount />
         <Emoji />
-        <Share />
+        <section className={css.shareLayout}>
+          <Share shareList={SHARE_OPTION_LIST} onClick={handleShareSucess} />
+          {showToast && (
+            <div className={css.toastBox}>
+              <Toast onClick={handleToastClose}>URL이 복사되었습니다.</Toast>
+            </div>
+          )}
+        </section>
       </div>
     </div>
   );
