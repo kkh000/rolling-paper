42a46da7554721eaedc494d22a6d7a775f2c7b02
JA_X <envijs93@gmail.com>
2024-03-04 13:35:19 +0900

    Feat: + 버튼 클릭 시 /post/id/message 페이지로 이동하는 핸들러함수 등록

--- src/pages/PostPage/UserPost/CardList/CardList.jsx
+++ src/pages/PostPage/UserPost/CardList/CardList.jsx
@@ -1,20 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import RoundedPlusButton from '../../../../components/Button/RoundedPlusButton';
 import Modal from '../../../../components/Modal/Modal';
 import { NONE_PORFILE_IMAGE } from '../../../../constant/constant';
+import createAxiosInstance from '../../../../utils/axios';
 import Card from './Card';
 import css from './CardList.module.scss';
 
+const testId = '4025';
 const CardList = () => {
   const [showModal, setShowModal] = useState(false);
+  const [messageData, setMessageData] = useState({});
+  const axios = createAxiosInstance();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(`/recipients/${testId}/messages/?limit=1000`);
+      setMessageData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(messageData);
   const toggleModal = () => setShowModal(!showModal);
+  const handleSendMessageClick = e => {
+    e.stopPropagation();
+    navigate(`/post/${testId}/message`);
+  };
   return (
     <div className={css.cardArea}>
       <div className={css.cardBox} onClick={toggleModal}>
         <div className={css.card}>
-          {/* onClick에 /post/{id}/message 로 가는 이벤트핸들러 넣어줘야됨 */}
-          {/* 얘를 클릭했을때는 모달이 나오면 안되기 때문에 나중에 e.stopPropagation(); 넣기 */}
-          <RoundedPlusButton />
+          <RoundedPlusButton onClick={e => handleSendMessageClick(e)} />
         </div>
         {/* data 받아서 map 돌릴예정 */}
         <Card />
