02bc4622a0c59deef2774c19c828caaa83ab7840
Yeojin Yoon <roseiris1001@gmail.com>
2024-03-04 09:51:50 +0900

    fix:프로필영역 불러오는 로직 변경, 오타 수정

--- src/components/Profiles/Profiles.jsx
+++ src/components/Profiles/Profiles.jsx
@@ -4,28 +4,21 @@ import css from './Profiles.module.scss';
 
 /**
  * 프로필 리스트와 사이즈를 받아서 그룹 프로필 리스트를 생성해준다
- * @param { ,Profiles, size }
+ * @param {any[]} Profiles
+ * @param {string} size
+ * -
  * - Profiles : []
  * - size : xSmall, small
- * @returns
  */
 const Profiles = ({ profileList, size = 'small' }) => {
-  const otherTotals = profileList.length - 3;
-
   return (
-    <div className={css.profilsArea}>
-      {profileList.map(
-        (profile, index) =>
-          index < 3 && (
-            <Profile
-              key={profile.id}
-              imgUrl={profile.profileImageURL}
-              zIndex={index + 1}
-              size={size}
-            />
-          ),
+    <div className={css.profilesArea}>
+      {profileList.slice(0, 3).map(profile => (
+        <Profile key={profile.id} imgUrl={profile.profileImageURL} size={size} />
+      ))}
+      {profileList.length > 3 && (
+        <div className={cn(css.writerCount, css[size])}>+{profileList.length - 3}</div>
       )}
-      {otherTotals > 0 && <div className={cn(css.writerCount, css[size])}>+{otherTotals}</div>}
     </div>
   );
 };
