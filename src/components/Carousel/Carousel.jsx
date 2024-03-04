971e90ad1709ed229331dbbd99d9394b7aae1e16
zoonyoung <9851248@gmail.com>
2024-03-04 10:07:54 +0900

    refactor: Modify Arrow 이미지 경로 constant추가 좌우 버튼 스타일 추가

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,7 +4,6 @@ import css from './Carousel.module.scss';
 
 const Carousel = ({ children }) => {
   const [slideIndex, setSlideIndex] = useState(0);
-
   const handleLeftButton = () => {
     if (slideIndex > 0) {
       setSlideIndex(slideIndex - 1);
@@ -18,19 +17,25 @@ const Carousel = ({ children }) => {
   };
 
   return (
-    <div className={css.carousel}>
-      <div className={css.slides} style={{ transform: `translateX(${-25 * slideIndex}%)` }}>
-        {Children.map(children, child => (
-          <div className={css.item}>{child}</div>
-        ))}
+    <section className={css.layout}>
+      <div className={css.carousel}>
+        <div className={css.slides} style={{ transform: `translateX(${-25 * slideIndex}%)` }}>
+          {Children.map(children, child => (
+            <div className={css.item}>{child}</div>
+          ))}
+        </div>
       </div>
-      <button className={css.leftButton} onClick={handleLeftButton}>
-        <img src={ARROW_LEFT_ICON} alt='arrow-left' />
-      </button>
-      <button className={css.rightButton} onClick={handleRightButton}>
-        <img src={ARROW_RIGHT_ICON} alt='arrow-right' />
-      </button>
-    </div>
+      {slideIndex > 0 && (
+        <button className={css.leftButton} onClick={handleLeftButton}>
+          <img src={ARROW_LEFT_ICON} />
+        </button>
+      )}
+      {slideIndex + 4 < children.length && (
+        <button className={css.rightButton} onClick={handleRightButton}>
+          <img src={ARROW_RIGHT_ICON} />
+        </button>
+      )}
+    </section>
   );
 };
 
