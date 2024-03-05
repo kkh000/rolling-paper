e5f58e277ab0e193536d9ced51102961fc61415c
KKH <gyooheon@naver.com>
2024-03-05 14:08:18 +0900

    feat : profile list 생성

--- src/pages/PostPage/MessagePage/ProfileImagePicker.jsx
+++ src/pages/PostPage/MessagePage/ProfileImagePicker.jsx
@@ -1,21 +1,23 @@
+
 import Profile from '../../../components/Profile/Profile';
-import ProfileList from '../../../components/Profiles/Profiles';
+import { PROFILE_IMAGE_URL_LIST } from '../../../constant/constant';
 import css from './ProfileImagePicker.module.scss';
 
-const ProfileImagePicker = ({ onChange }) => {
-  const handleProfile = url => {
-    onChange(url);
-  };
+
+const ProfileImagePicker = () => {
+  const urlList = PROFILE_IMAGE_URL_LIST;
+
   return (
     <section className={css.box}>
       <h1 className={css.title}>프로필 이미지</h1>
       <div className={css.profileImage}>
-        <Profile className={css.defaultImage} />
+        <Profile imgUrl={PROFILE_IMAGE_URL_LIST[0]}  />
         <div className={css.profileBox}>
           <h2 className={css.selectProfile}>프로필 이미지를 선택해주세요!</h2>
-          <div className={css.imageList}>
-            <ProfileList onChange={handleProfile} className={css.image} />
-          </div>
+          {urlList.map((imageUrl, index) => (
+            <Profile key={index} imgUrl={imageUrl}  />
+          ))}
+          <div className={css.imageList}></div>
         </div>
       </div>
     </section>
