c20e6999ffb23cfe04ebe2079940cb6866c89fc6
JA_X <envijs93@gmail.com>
2024-03-08 22:48:14 +0900

    Feat: Edit 페이지 기능 구현

--- src/pages/PostPage/UserPost/CardList/Card.jsx
+++ src/pages/PostPage/UserPost/CardList/Card.jsx
@@ -1,22 +1,67 @@
+import { useState } from 'react';
 import Badge from '../../../../components/Badge/Badge';
 import Profile from '../../../../components/Profile/Profile';
+import { DELETE_ICON_PATH } from '../../../../constant/constant';
+import { ARROW_ROTATE_ICON } from '../../../../constant/constant';
+import { cn } from '../../../../utils/classNames';
 import { createdDate } from '../../../../utils/createdDate';
 import css from './Card.module.scss';
 
-const Card = ({ content, createdAt, font, profileImageURL, relationship, sender, onClick }) => {
+const Card = ({
+  id,
+  content,
+  createdAt,
+  font,
+  profileImageURL,
+  relationship,
+  sender,
+  onClick,
+  onAddId,
+  selectedMessageIdList,
+  isEditing,
+}) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const selectedCardClassName = cn(css.layout, isSelected && css.selected);
+
   const fontStyle = {
     fontFamily: font,
   };
+
+  const handleDeletedIdList = id => {
+    if (selectedMessageIdList.includes(id)) {
+      const newIdList = selectedMessageIdList.filter(prevId => prevId !== id);
+      onAddId(newIdList);
+    } else {
+      onAddId([...selectedMessageIdList, id]);
+    }
+  };
+
   return (
-    <div className={css.layout} onClick={onClick}>
+    <div className={selectedCardClassName} onClick={onClick}>
       <section className={css.header}>
         <Profile imgUrl={profileImageURL} />
         <div className={css.profile}>
-          <p>
-            From. <span className={css.name}>{sender}</span>
+          <p className={css.name}>
+            <span className={css.from}>From.</span> {sender}
           </p>
           <Badge relationship={relationship} />
         </div>
+        {isEditing && (
+          <div
+            className={css.deleteButton}
+            onClick={e => {
+              e.stopPropagation();
+              handleDeletedIdList(id);
+              setIsSelected(!isSelected);
+            }}
+          >
+            {isSelected ? (
+              <img src={ARROW_ROTATE_ICON} alt='되돌리기' />
+            ) : (
+              <img src={DELETE_ICON_PATH} alt='삭제' />
+            )}
+          </div>
+        )}
       </section>
       <div className={css.divider} />
       <div className={css.content}>
