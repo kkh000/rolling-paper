1c1efcd93387b78c20d4f930c00ae82c55d84512
zoonyoung <9851248@gmail.com>
2024-03-09 18:53:55 +0900

    feat: Implement bar 이름, 작성한 사람 구현

--- src/pages/PostPage/UserPost/Post.jsx
+++ src/pages/PostPage/UserPost/Post.jsx
@@ -1,11 +1,26 @@
+import { useParams } from 'react-router-dom';
+import useFetchData from '../../../hooks/useFetchData';
 import Bar from './Bar/Bar';
 import CardList from './CardList/CardList';
 import css from './Post.module.scss';
 const Post = () => {
+  const { id } = useParams();
+  const { data: response, isLoading } = useFetchData(`recipients/${id}/`);
+
+  if (isLoading) {
+    return <div>Ladoing...</div>;
+  }
+
+  if (!response) {
+    return <div>no Data</div>;
+  }
+
+  const { name, recentMessages, messageCount, backgroundColor, backgroundImageURL } = response;
+
   return (
     <div className={css.layout}>
-      <Bar />
-      <CardList />
+      <Bar name={name} messages={recentMessages} messageCount={messageCount} />
+      <CardList backgroundColor={backgroundColor} backgroundImageURL={backgroundImageURL} />
     </div>
   );
 };
