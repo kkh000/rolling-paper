c3687d5c9964e75791dcc5b689b0d2e8e7c74a3b
zoonyoung <9851248@gmail.com>
2024-03-10 23:15:52 +0900

    feat: 검색기능 추가(autoSuggestion 기능 탑재)

---
+++ src/components/AutoSearching/AutoSearching.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useOutsideClick from '../../hooks/useOutsideClick';
+import Button from '../Button/Button';
+import css from './AutoSearching.module.scss';
+
+const AutoSearching = ({ dataList = [] }) => {
+  const [keyword, setKeyword] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const [suggestionList, setSuggestionList] = useState([]);
+  const [showAutoSuggestion, setShowAutoSuggestion] = useState(false);
+  const autoSuggestionRef = useRef(null);
+  const navigate = useNavigate();
+
+  useOutsideClick(autoSuggestionRef, () => setShowAutoSuggestion(false));
+
+  const handleSuggestionClick = event => {
+    setKeyword(event.target.id);
+    setShowAutoSuggestion(false);
+  };
+
+  const handleClick = event => {
+    event.preventDefault();
+    navigate('/search', { state: { data: filteredData } });
+  };
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      if (!keyword) return setSuggestionList([]);
+      const nameSet = new Set();
+      const newFilteredData = dataList.filter(data => {
+        if (data.name.includes(keyword)) {
+          nameSet.add(data.name);
+          return true;
+        }
+      });
+      setSuggestionList([...nameSet]);
+      setFilteredData(newFilteredData);
+    }, 200);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [keyword]);
+
+  return (
+    <form className={css.layout} ref={autoSuggestionRef}>
+      <input
+        className={css.input}
+        type='text'
+        value={keyword}
+        onFocus={() => {
+          setShowAutoSuggestion(true);
+        }}
+        onChange={e => setKeyword(e.target.value)}
+      />
+      <Button type='submit' fill={false} size='s' className={css.button} onClick={handleClick}>
+        search
+      </Button>
+      {showAutoSuggestion && (
+        <ul className={css.itemBox}>
+          {suggestionList.map(data => (
+            <li
+              className={css.item}
+              key={data}
+              id={data}
+              onClick={handleSuggestionClick}
+              dangerouslySetInnerHTML={{
+                __html: data.replace(new RegExp(`(${keyword})`, 'gi'), '<span>$1</span>'),
+              }}
+            />
+          ))}
+          {suggestionList.length === 0 && <li className={css.item}>검색된 결과가 없습니다.</li>}
+        </ul>
+      )}
+    </form>
+  );
+};
+
+export default AutoSearching;
